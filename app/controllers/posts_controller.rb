f2183be5bf6820e9a42dc5f67831139242d82e11
jomapormentilla <jomapormentilla@gmail.com>
2021-01-05 21:02:16 -0500

    commit tree f4b44c3e087a5eb45a3129f5579ad8dad59ae4fc

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @post.comments.build(content: nil)
+    @comment = Comment.new
   end
 
   def index
